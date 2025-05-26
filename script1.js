d43275850730e9581f137f6adfccc22d8bd478f4
alvaro725 <alvaro.olivares16@inacapmail.cl>
2025-05-26 11:52:49 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,2 +1,10 @@
 //validar nombre, debe contener solo letras y no ser vacio
+function validar(){
+    enombre=document.getElementById(Nombre)
+    
+}
+function eliminar(){
+    
+
+}
 //validar correo: Debe tener solo un @ y terminar en .cl
